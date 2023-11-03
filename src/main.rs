91d0fcc1afa111c5a8f92b230a9a8503b47aeb21
Vimal V Nair <vimalvnair999@gmail.com>
2023-11-03 19:23:43 +0530

    Save notion links to a file

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,93 @@
-use std::env;
-use serde::{Deserialize, Serialize};
+mod notion;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct NotionPage{
-    results: Vec<NotionResult>,
-    has_more: bool,
-    next_cursor: String
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename(deserialize = "Result"))]
-struct NotionResult{
-    id: String,
-    properties: Properties
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Properties{
-    #[serde(rename(deserialize = "URL"))]
-    url: Url,
-    #[serde(rename(deserialize = "Name"))]
-    name: Name
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Url{
-    url: String
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Name{
-    title: Vec<Title>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Title{
-    text: Content
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Content{
-    content: String
-}
+use std::{env, fs::File, io::Write};
+use notion::NotionPage;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let notion_links = get_notion_links().await?;
+    save_notion_links_to_file(&notion_links);
+
+    Ok(())
+}
+
+async fn get_notion_links() -> Result<Vec<notion::NotionLink>, Box<dyn std::error::Error>>{
+    let mut request_params = notion::Params{
+        sorts: vec![
+            notion::Sort{
+                property: "Created".to_owned(),
+                direction: "descending".to_owned()
+            }
+        ],
+        start_cursor: None
+    };
+
+    let mut notion_pages = notion::NotionPages{
+        pages: Vec::new()
+    };
+
+    let mut notion_links: Vec<notion::NotionLink> = Vec::new();
+
+    let mut has_more = true;
+
+    while has_more{
+        println!("Request params: {:?}", request_params);
+
+        let response: NotionPage = get_notion_page(&request_params).await?;
+
+        println!("first result : {:?}", response.results.get(0).unwrap());
+        println!("cursor = {:?}", response.next_cursor);
+        println!("result count = {}", response.results.len());
+
+
+        has_more = response.has_more;
+
+        if has_more{
+            request_params.start_cursor = Some(response.next_cursor.clone().unwrap())
+        }
+
+        let links: Vec<notion::NotionLink> =  response.results.iter().map(|result| {
+            let id = &result.id;
+            let url = &result.properties.url.url;
+            let title = &result.properties.name.title.get(0).unwrap().text.content;
+
+            notion::NotionLink{
+                id: id.to_string(),
+                url: url.to_string(),
+                title: title.to_string()
+            }
+        }).collect();
+
+        notion_links.extend(links);
+
+        notion_pages.pages.push(response);
+    }
+
+    Ok(notion_links)
+}
+
+async fn get_notion_page(request_params: &notion::Params) -> Result<NotionPage, Box<dyn std::error::Error>>{
     let notion_api_key = env::var("NOTION_API_KEY").unwrap();
     let notion_database_id = env::var("NOTION_DATABASE_ID").unwrap();
 
     let url = format!("https://api.notion.com/v1/databases/{notion_database_id}/query");
 
+    println!("making request to: {}", url);
+
     let response: NotionPage = reqwest::Client::new()
         .post(&url)
+        .json(&request_params)
         .bearer_auth(&notion_api_key)
         .send()
         .await?
         .json()
         .await?;
 
-    println!("making request to: {}", url);
-    println!("first result : {:?}", response.results.get(0).unwrap());
-    println!("cursor = {:?}", response.next_cursor);
-    println!("result count = {}", response.results.len());
+    Ok(response)
+}
 
-    Ok(())
+fn save_notion_links_to_file(notion_links: &Vec<notion::NotionLink>){
+    let links = serde_json::to_string(notion_links).unwrap();
+    let mut file = File::create("notion_links.json").unwrap();
+    file.write_all(links.as_bytes()).unwrap();
 }
---
+++ src/notion.rs
@@ -0,0 +1,68 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NotionPages{
+    pub pages: Vec<NotionPage>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NotionLink{
+    pub id: String,
+    pub url: String,
+    pub title: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NotionPage{
+    pub results: Vec<NotionResult>,
+    pub has_more: bool,
+    pub next_cursor: Option<String>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename(deserialize = "Result"))]
+pub struct NotionResult{
+    pub id: String,
+    pub properties: Properties
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Properties{
+    #[serde(rename(deserialize = "URL"))]
+    pub url: Url,
+    #[serde(rename(deserialize = "Name"))]
+    pub name: Name
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Url{
+    pub url: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Name{
+   pub title: Vec<Title>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Title{
+    pub text: Content
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Content{
+    pub content: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Params{
+    pub sorts: Vec<Sort>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub start_cursor: Option<String>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Sort{
+    pub property: String,
+    pub direction: String
+}
